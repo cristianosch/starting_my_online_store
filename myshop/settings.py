bd9487e1429b20b3403d8b4c068f7b8875a9ecb8
Cristiano Schroeder <webdesignercristiano@gmail.com>
2022-09-28 02:20:55 +0100

    adding shopping cart

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -122,5 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+CART_SESSION_ID = 'cart' 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from cart.forms import CartAddProductForm
 
 # Create your views here.
 
@@ -25,8 +26,10 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
-    
+    cart_product_form = CartAddProductForm()
+
     return render(request,
                   'shop/product/detail.html',
-                  {'product': product})
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
                    
